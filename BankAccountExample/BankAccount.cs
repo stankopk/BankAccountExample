0f6dd86f61cb7a6c209f852c6c036bbb54f6ab95
Stanko Yanev <stanko.yanev@edu.mon.bg>
2023-05-05 11:14:50 +0300

    Add project files.

---
+++ BankAccountExample/BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountExample
+{
+    public class BankAccount
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+
+        public BankAccount()
+        {
+            //constructor
+        }
+    }
+}
---
+++ BankAccountExample/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountExample
+{
+    internal class Program
+    {
+        static List<BankAccount> bankAccounts = new List<BankAccount>();
+
+        public static void Create(int id)
+        {
+            var existingbankAccount = bankAccounts.Where(b => b.Id == id).FirstOrDefault();
+            if(existingbankAccount != null)
+            {
+                Console.WriteLine("Account already exists!");
+            }
+            else
+            {
+                var acc = new BankAccount();
+                acc.Id = id;
+                bankAccounts.Add(acc);
+            }
+        }
+
+        public static void Deposit(int id, double amount)
+        {
+            var existingbankAccount = bankAccounts.Where(b => b.Id == id).FirstOrDefault();
+            if (existingbankAccount == null)
+            {
+                Console.WriteLine("Account doesn't exists!");
+            }
+            else
+            {
+                existingbankAccount.Amount += amount;
+            }
+        }
+
+        public static void Withdraw(int id, double amount)
+        {
+            var existingbankAccount = bankAccounts.Where(b => b.Id == id).FirstOrDefault();
+            if (existingbankAccount == null)
+            {
+                Console.WriteLine("Account doesn't exists!");
+            }
+            else
+            {
+                if(existingbankAccount.Amount >= amount)
+                {
+                    existingbankAccount.Amount -= amount;
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient balance");
+                }
+            }
+        }
+
+        public static void Print(int id)
+        {
+            var existingbankAccount = bankAccounts.Where(b => b.Id == id).FirstOrDefault();
+            if (existingbankAccount == null)
+            {
+                Console.WriteLine("Account doesn't exists!");
+            }
+            else
+            {
+                Console.WriteLine("Account ID"+ existingbankAccount.Id + 
+                    ", balance " + String.Format("{0:0.00}", existingbankAccount.Amount));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Create 1
+            string command = Console.ReadLine();
+            while(command != "End")
+            {
+                var cmdArgs = command.Split(' ');
+                var cmdType = cmdArgs[0];
+                switch(cmdType)
+                {
+                    case "Create":
+                        Create(int.Parse(cmdArgs[1]));
+                        break;
+                    case "Deposit":
+                        Deposit(int.Parse(cmdArgs[1]), double.Parse(cmdArgs[2]));
+                        break;
+                    case "Withdraw":
+                        Withdraw(int.Parse(cmdArgs[1]), double.Parse(cmdArgs[2]));
+                        break;
+                    case "Print":
+                        Print(int.Parse(cmdArgs[1]));
+                        break;
+                    default:
+                        Console.WriteLine("Error! Wrong Command");
+                        break;
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
